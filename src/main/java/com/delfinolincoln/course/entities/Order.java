710ca83a820f2423073b814b653bfd97c03aca03
thelastvascaino <lincolncdelfino@gmail.com>
2026-02-01 20:14:28 -0300

    Annotation @JsonIgnore added in User and Order

--- src/main/java/com/delfinolincoln/course/entities/Order.java
+++ src/main/java/com/delfinolincoln/course/entities/Order.java
@@ -3,6 +3,8 @@ package com.delfinolincoln.course.entities;
 import java.io.Serializable;
 import java.time.Instant;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -22,6 +24,7 @@ public class Order implements Serializable {
     private Instant moment;
 
     /*<associations>*/
+    @JsonIgnore
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;

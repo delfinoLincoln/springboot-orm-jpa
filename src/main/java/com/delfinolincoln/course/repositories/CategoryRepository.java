0e78c024e86bdcda8a7db1d34e3cf45fb5c27eae
thelastvascaino <lincolncdelfino@gmail.com>
2026-02-01 21:33:30 -0300

    Entity Category, CategoryRepository, CategoryService and CategoryResource added and test profile instantiating OrderRepository

---
+++ src/main/java/com/delfinolincoln/course/repositories/CategoryRepository.java
@@ -0,0 +1,11 @@
+package com.delfinolincoln.course.repositories;
+
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.delfinolincoln.course.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+    
+}

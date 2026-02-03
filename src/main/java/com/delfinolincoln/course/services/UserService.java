14b3893d9ec53f8f4ea4e98ec77bcf5c4e9a8995
thelastvascaino <lincolncdelfino@gmail.com>
2026-02-03 17:29:41 -0300

    Exception handling - update

--- src/main/java/com/delfinolincoln/course/services/UserService.java
+++ src/main/java/com/delfinolincoln/course/services/UserService.java
@@ -13,6 +13,8 @@ import com.delfinolincoln.course.repositories.UserRepository;
 import com.delfinolincoln.course.services.exceptions.DataBaseException;
 import com.delfinolincoln.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
     @Autowired
@@ -44,9 +46,14 @@ public class UserService {
     }
 
     public User update(Long id, User obj) {
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        }
+        catch(EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User obj) {

dd6f7121cb45a780d9ce20806d6493d9ef52e541
thelastvascaino <lincolncdelfino@gmail.com>
2026-02-03 14:45:21 -0300

    Entity payment created and OneToOne association with Order

--- src/main/java/com/delfinolincoln/course/config/TestConfig.java
+++ src/main/java/com/delfinolincoln/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.delfinolincoln.course.entities.Category;
 import com.delfinolincoln.course.entities.Order;
 import com.delfinolincoln.course.entities.OrderItem;
+import com.delfinolincoln.course.entities.Payment;
 import com.delfinolincoln.course.entities.Product;
 import com.delfinolincoln.course.entities.User;
 import com.delfinolincoln.course.entities.enums.OrderStatus;
@@ -79,5 +80,10 @@ public class TestConfig implements CommandLineRunner {
         OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
         orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));	
+
+        Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+        o1.setPayment(pay1);
+
+        orderRepository.save(o1);
     }
 }
